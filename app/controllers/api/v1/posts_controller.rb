a32e8f86bae5629a2b2da6ad76da563d24136688
Daniel Ionita <danielionita88@yahoo.com>
2020-01-14 10:06:34 -0600

    create posts with lat and lng

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -25,6 +25,6 @@ class Api::V1::PostsController < ApplicationController
     private
 
     def post_params
-        params.permit(:user_id, :title, :make, :model, :model_year, :price, :odometer, :title_status, :city, :description, {photos: []})
+        params.permit(:user_id, :title, :make, :model, :model_year, :price, :odometer, :title_status, :address, :lat, :lng, :description, {photos: []})
     end
 end
--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,6 +1,6 @@
 class PostSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
-  attributes :id, :user_id, :title, :make, :model, :model_year, :price, :odometer, :title_status, :city, :description, :photos_urls
+  attributes :id, :user_id, :title, :make, :model, :model_year, :price, :odometer, :title_status, :description,:address,:lat,:lng,:photos_urls
 
   def photos_urls
     object.photos.map{|photo| rails_blob_url(photo, only_path:true)}
