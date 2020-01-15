32aa97cc3b38f40eae4fc5a66c3ce793e2502689
Daniel Ionita <danielionita88@yahoo.com>
2020-01-15 11:51:55 -0600

    create likes and remove

--- app/controllers/api/v1/likes_controller.rb
+++ app/controllers/api/v1/likes_controller.rb
@@ -1,2 +1,24 @@
 class Api::V1::LikesController < ApplicationController
+
+    def create
+        like=Like.new(like_params)
+        if like.save
+            render json: like
+        else
+            render json:{message: 'invalid like'}
+        end
+    end
+
+    def remove_like
+        like=Like.find_by(user_id: params[:user_id], post_id: params[:post_id]).destroy
+        render json: {message: 'delete succesful'}
+    end
+
+
+    private
+
+    def like_params
+        params.require(:like).permit(:user_id, :post_id)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,12 +5,14 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :users, only: [:index, :create]
       resources :posts, only: [:index, :create, :show, :destroy]
+      resources :likes, only: [:create, :destroy]
       get 'users/:id/liked_posts', to: 'users#liked_posts'
       get 'users/:id/posts', to: 'users#posts'
       post '/login', to: 'users#login'
       post '/signup', to: 'users#create'
       get '/current_user', to: 'users#show'
       get '/google_maps', to: 'posts#google_maps'
+      delete '/likes/:user_id/:post_id', to: 'likes#remove_like'
     end
   end
 end
