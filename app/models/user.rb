9c135aa7789b3485eb3f45c01617ec43a8c598ca
Daniel Ionita <danielionita88@yahoo.com>
2020-01-17 13:32:29 -0600

    email and user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
     has_many :likes, dependent: :destroy
     has_many :liked_posts, through: :likes, source: :post
     has_secure_password
-    validates :username, uniqueness: {case_sensitive: false}
+    validates :username, uniqueness: {case_sensitive: false}, length: {minimum:3}
+    validates :email, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
   
 end
