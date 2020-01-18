486e1a8782c29ddb01b19889169c781926a525be
Daniel Ionita <danielionita88@yahoo.com>
2020-01-18 10:09:33 -0600

    email uniq validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
     has_many :liked_posts, through: :likes, source: :post
     has_secure_password
     validates :username, uniqueness: {case_sensitive: false}, length: {minimum:3}
-    validates :email, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
+    validates :email, format: {with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}, uniqueness: true
   
 end
